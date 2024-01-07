18088d7364cba2ad6927c6093cc74a10e43d466a
hariom <hk524569@gmail.com>
2024-01-07 15:37:27 +0530

    commited 1/4/24

--- bitmanipulation/BitWiseAndRange.cpp
+++ bitmanipulation/BitWiseAndRange.cpp
@@ -1,14 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 int solve(int left,int right){
+    //optimal solution
     while(left<right){
         right=right&(right-1);
     }
     return right;
 }
+int solve2(int left,int right){
+    int c=0;
+    while(left!=right){
+        left=left<<1;
+        right=right<<1;
+    }
+    return right*(1<<c);
+}
 int main(){
     int left,right;
     cin>>left>>right;
     int ans=solve(left,right);
-    cout<<ans;
+    int ans2=solve(left,right);
+    cout<<ans<<endl;
+    cout<<ans2<<endl;
 }
